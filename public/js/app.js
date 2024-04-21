4279839babf5b16f6011a70a9fa00ffae989d701
Ozz Dm <ojaswideshmukh23@gmail.com>
2024-04-21 21:29:52 +0530

    Heroku/Render changes

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageOne.textContent = 'Loading ...'
     messageTwo.textContent = ''
     
-    fetch(`http://localhost:3000/weather-app?address=${location}`).then((response)=>{
+    fetch(`/weather-app?address=${location}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error)
             {
